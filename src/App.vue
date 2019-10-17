a56f07a4c887ffb54f35ec76d1f1d0e3ed7b9601
wlj <251218146@qq.com>
2019-10-17 14:26:59 +0800

    修改了购物车图标

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="app-container">
     <h1>123</h1>
+    <!-- 在Mint UI中的组件 -->
     <mt-header fixed title="vue项目"></mt-header>
+   
+   
+   <!-- 在mui中引入的代码 -->
     <nav class="mui-bar mui-bar-tab">
       <a class="mui-tab-item mui-active" href="#tabbar">
         <span class="mui-icon mui-icon-home"></span>
         <span class="mui-tab-label">首页</span>
       </a>
       <a class="mui-tab-item" href="#tabbar-with-chat">
-        <span class="mui-icon mui-icon-email">
-          <span class="mui-badge">9</span>
-        </span>
-        <span class="mui-tab-label">消息</span>
+      <span class="mui-icon mui-icon-contact"></span>
+        <span class="mui-tab-label">会员</span>
       </a>
       <a class="mui-tab-item" href="#tabbar-with-contact">
-        <span class="mui-icon mui-icon-contact"></span>
-        <span class="mui-tab-label">通讯录</span>
+        
+        <!-- 图标 -->
+          <span class="mui-icon mui-icon-extra mui-icon-extra-cart">
+          <span class="mui-badge">0</span>
+        </span>
+        <span class="mui-tab-label">购物车</span>
       </a>
       <a class="mui-tab-item" href="#tabbar-with-map">
-        <span class="mui-icon mui-icon-gear"></span>
-        <span class="mui-tab-label">设置</span>
+        <span class="mui-icon mui-icon-search"></span>
+        <span class="mui-tab-label">搜索</span>
       </a>
     </nav>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 
 import Vue from 'vue'
 import './lib/MUI/css/mui.min.css'
+import './lib/MUI/css/icons-extra.css'
+
 import App from './App.vue'
 import {Header} from 'mint-ui'
 Vue.component(Header.name,Header)
@@ -8,7 +10,6 @@ Vue.component(Header.name,Header)
 
 
 var vm =new Vue({
-
     el:"#app",
     data:{},
     render:c=>c(App)
