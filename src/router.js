b7c01503d91050019a344a6ae7e93a5bff242139
wlj <251218146@qq.com>
2019-11-28 18:11:51 +0800

    懒加载

--- src/router.js
+++ src/router.js
@@ -1,17 +1,28 @@
 //再次导入vuerouter因为需要新建router实例
 import VueRouter from 'vue-router'
-//导入需要用的路由组件
-import shopcar from './components/tabbar/shopcar.vue'
-import search from './components/tabbar/search.vue'
-import member from './components/tabbar/member.vue'
-import home from './components/tabbar/home.vue'
-import news from './components/news/news.vue'
-import newsInfo from './components/news/newsInfo.vue'
+//1.导入需要用的路由组件
+// import shopcar from './components/tabbar/shopcar.vue'
+// import search from './components/tabbar/search.vue'
+// import member from './components/tabbar/member.vue'
+// import home from './components/tabbar/home.vue'
+// import news from './components/news/news.vue'
+// import newsInfo from './components/news/newsInfo.vue'
+// import goodslist from './components/goods/goodslist.vue'
+// import photolist from './components/photos/photoList.vue'
+// import photoinfo from './components/photos/photoinfo.vue'
+// import goodslistinfo from './components/goods/goodslistinfo.vue'
+//2.采用懒加载进行组件动态导入
+const shopcar = () => import('./components/tabbar/shopcar.vue');
+const search=()=>import('./components/tabbar/search.vue');
+const member=()=>import('./components/tabbar/member.vue');
+const home=()=>import('./components/tabbar/home.vue');
+const news=()=>import('./components/news/news.vue');
+const newsInfo=()=>import('./components/news/newsInfo.vue');
+const goodslist=()=>import('./components/goods/goodslist.vue');
+const photolist=()=>import('./components/photos/photoList.vue');
+const photoinfo=()=>import('./components/photos/photoinfo.vue');
+const goodslistinfo=()=>import('./components/goods/goodslistinfo.vue');
 
-import goodslist from './components/goods/goodslist.vue'
-import photolist from './components/photos/photoList.vue'
-import photoinfo from './components/photos/photoinfo.vue'
-import goodslistinfo from './components/goods/goodslistinfo.vue'
 var router = new VueRouter({
 
     routes: [
@@ -28,7 +39,7 @@ var router = new VueRouter({
         { path: '/home/photolist', component: photolist },
 
         { path: '/home/photoinfo/:id', component: photoinfo },
-        { path: '/home/goodslistinfo/:id', component: goodslistinfo,name:'goodslistinfo'},
+        { path: '/home/goodslistinfo/:id', component: goodslistinfo, name: 'goodslistinfo' },
     ],
     linkActiveClass: 'mui-active'//覆盖默认路由高亮的类
 })
